f12bd1908de02eec258d514974ac5aa8d35355dd
Felipe Schoffen <fschoffen@yahoo.com>
2023-08-15 17:06:00 -0300

    feat(EmbeddedElements): show current indicator value

--- app/src/main/java/com/example/letskillwithcompose/projects/KindaInternetSpeedCheck.kt
+++ app/src/main/java/com/example/letskillwithcompose/projects/KindaInternetSpeedCheck.kt
@@ -1,13 +1,17 @@
 package com.example.letskillwithcompose.projects
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -24,8 +29,11 @@ import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -58,19 +66,38 @@ fun CircularIndicator(
     backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = .1f),
     backgroundIndicatorStrokeWidth: Float = 100f,
     foregroundIndicatorColor: Color = MaterialTheme.colorScheme.primary,
-    foregroundIndicatorStrokeWidth: Float = 100f
+    foregroundIndicatorStrokeWidth: Float = 100f,
+    bigTextFontSize: TextUnit = MaterialTheme.typography.headlineLarge.fontSize,
+    bigTextColor: Color = MaterialTheme.colorScheme.onSurface,
+    bigTextSuffix: String = "GB",
+    smallText: String = "Remaining",
+    smallTextFontSize: TextUnit = MaterialTheme.typography.labelLarge.fontSize,
+    smallTextColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = .3f)
 ) {
-    val animatedIndicatorValue = remember { Animatable(initialValue = 0f) }
+    var animatedIndicatorValue by remember { mutableStateOf(0f) }
     LaunchedEffect(key1 = indicatorValue) {
-        animatedIndicatorValue.animateTo(indicatorValue.toFloat())
+        animatedIndicatorValue = indicatorValue.toFloat()
     }
 
     val percentage =
-        ((animatedIndicatorValue.value / maxIndicatorValue) * 100).coerceAtMost(100f)
+        ((animatedIndicatorValue / maxIndicatorValue) * 100).coerceAtMost(100f)
 
     val sweepAngle by animateFloatAsState(
         targetValue = (2.4 * percentage).toFloat(),
         animationSpec = tween(1000), label = ""
+    )
+
+    val receivedValue by animateIntAsState(
+        targetValue = indicatorValue,
+        animationSpec = tween(1000), label = ""
+    )
+
+    val animatedBigTextColor by animateColorAsState(
+        targetValue = if (indicatorValue == 0) {
+            MaterialTheme.colorScheme.onSurface.copy(alpha = .3f)
+        } else bigTextColor,
+        label = "",
+        animationSpec = tween(1000)
     )
 
     Column(
@@ -89,9 +116,19 @@ fun CircularIndicator(
                     indicatorColor = foregroundIndicatorColor,
                     indicatorStrokeWidth = foregroundIndicatorStrokeWidth
                 )
-            }
+            },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        EmbeddedElements(
+            bigText = receivedValue,
+            bigTextFontSize = bigTextFontSize,
+            bigTextColor = animatedBigTextColor,
+            bigTextSuffix = bigTextSuffix,
+            smallText = smallText,
+            smallTextColor = smallTextColor,
+            smallTextFontSize = smallTextFontSize
+        )
     }
 }
 
@@ -137,5 +174,30 @@ fun DrawScope.foregroundIndicator(
             x = (size.width - componentSize.width) / 2f,
             y = (size.height - componentSize.height) / 2f,
         )
+    )
+}
+
+@Composable
+fun EmbeddedElements(
+    bigText: Int,
+    bigTextFontSize: TextUnit,
+    bigTextColor: Color,
+    bigTextSuffix: String,
+    smallText: String,
+    smallTextColor: Color,
+    smallTextFontSize: TextUnit
+) {
+    Text(
+        text = smallText,
+        color = smallTextColor,
+        fontSize = smallTextFontSize,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = "$bigText ${bigTextSuffix.take(2)}",
+        color = bigTextColor,
+        fontSize = bigTextFontSize,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
     )
 }
